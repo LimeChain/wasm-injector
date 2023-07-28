0f4cf32fa46b3b76be14050778684c12dfaa7eba
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2023-07-28 10:38:48 +0300

    feat(scripts): Update julia script
    Signed-off-by: Maksim Dimitrov <dimitrov.maksim@gmail.com>

--- scripts/runTests.jl
+++ scripts/runTests.jl
@@ -1,38 +1,27 @@
 # Define the hosts, on which the tests will be run
-hosts  = ["polkadot", "kagome"]
+hosts::Vector{String} = ["polkadot", "kagome"]
 
 # Add locally built or downloaded adapters, testers, and hosts to PATH
 ENV["PATH"] *= ":$(pwd())/bin"
 
 # Tests directory
-tests_dir = get(ENV ,"ZN_TESTS", "./tests")
+tests_dir::String = get(ENV ,"ZN_TESTS", "./tests")
 
 # Test output path
-tests_output = get(ENV, "ZN_TEST_OUTPUT", "./tests_output")
-
-# Filter out the non-`.zndsl` files, just to be safe
-tests = filter(file -> endswith(file, ".zndsl"), readdir(tests_dir))
+tests_output::String = get(ENV, "ZN_TEST_OUTPUT", "./tests_output")
 
 # Arrays to accumulate passed and failed test names
-passed_tests = []
-failed_tests = []
-
-# Helper for piping both outputs
-function redirect_stdout_stderr(dofunc, outfile, errfile)
-    open(outfile, "w") do out
-        open(errfile, "w") do err
-            redirect_stdout(out) do
-                redirect_stderr(err) do
-                    dofunc()
-                end
-            end
-        end
-    end
-end
+passed_tests::Vector{String} = []
+failed_tests::Vector{String} = []
 
 # For each host ...
-for host in hosts
+for host::String in hosts
     withenv("ZOMBIENET_DEFAULT_START_COMMAND" => host) do
+        # Get host tests directory
+        host_tests_dir::String = joinpath(tests_dir, host)
+        # Filter out the non-`.zndsl` files for each host
+        tests::Vector{String} = filter(file -> endswith(file, ".zndsl"), readdir(host_tests_dir))
+        
         # ... run each test
         for test::String in tests
             if endswith(test, ".zndsl")
@@ -42,25 +31,21 @@ for host in hosts
                 test_name::String = match_captures[2]
 
                 # Prepare the `zombienet test` command
-                command::Cmd = `node zombienet/javascript/packages/cli/dist/cli.js --provider native test $(joinpath(tests_dir, test))`
-                println("Running test [$(host)] $(test_name)")
+                command::Cmd = `zombienet -p native test $(joinpath(host_tests_dir, test))`
+                full_test_name::String = "[$(host)] $(test_name)"
+                println("Running test $(full_test_name)")
 
                 # Try to run the test
                 try
-                    # Create the output files
-                    output_stdout = joinpath(tests_output, "output_$(index)-$(test_name)_stdout.log")
-                    output_stderr = joinpath(tests_output, "output_$(index)-$(test_name)_stderr.log")
-
                     # Try to run the command and capture its output
-                    redirect_stdout_stderr(output_stdout, output_stderr) do
-                        run(command)
-                    end
+                    (st, pr) = readsfrom(run(command))
+                    readlines(st)
 
                     # Add the test name to the passed tests array
-                    push!(passed_tests, test_name)
+                    push!(passed_tests, full_test_name)
                 catch e
                     # Add the test name to the failed tests array
-                    push!(failed_tests, test_name)
+                    push!(failed_tests, full_test_name)
                 end
             end
         end
@@ -73,3 +58,8 @@ println(passed_tests)
 
 println("Failed tests:")
 println(failed_tests)
+
+# Exit with an error code if any tests failed
+if length(failed_tests) > 0
+    exit(1)
+end
