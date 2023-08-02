2c670dc7a5410cdda8663d8323241c0b088c1cb5
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2023-08-02 15:47:41 +0300

    refactor(cli)!: Remove nothing injection. Add inject and decode subcommads
    Signed-off-by: Maksim Dimitrov <dimitrov.maksim@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,120 @@
-use clap::{Parser, ValueHint};
+use clap::{Parser, Subcommand, ValueHint};
 use std::path::PathBuf;
 use wasm_injector::injecting::injections::Injection;
 use wasm_injector::util::{load_module_from_wasm, modify_file_name, save_module_to_wasm};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
+#[group()]
 struct Args {
-    #[arg(index = 1, required = true, value_name = "injection")]
-    injection: Injection,
+    #[command(subcommand)]
+    action: Action,
+}
 
-    #[arg(index = 2, required = true, value_name = "(hexified) (compressed) wasm source file", value_hint = ValueHint::FilePath)]
+#[derive(Debug, Subcommand)]
+enum Action {
+    Inject {
+        #[arg(required = true, value_name = "injection", value_hint = ValueHint::Other)]
+        injection: Injection,
+
+        #[arg(
+            long,
+            value_name = "compressed",
+            help = "Compresses the wasm. Can be combined with `hexified`",
+            default_value_t = false
+        )]
+        compressed: bool,
+
+        #[arg(
+            long,
+            value_name = "hexified",
+            help = "Hexifies the wasm. Can be combined with `compressed`",
+            default_value_t = false
+        )]
+        hexified: bool,
+        #[command(flatten)]
+        global_opts: GlobalOpts,
+    },
+    Decode {
+        #[command(flatten)]
+        global_opts: GlobalOpts,
+    },
+}
+
+#[derive(Parser, Debug, Clone)]
+struct GlobalOpts {
+    #[arg(required = true, help = "Wasm source file path. Can be compressed and/or hexified.", value_hint = ValueHint::FilePath)]
     source: PathBuf,
 
-    #[arg(index = 3, value_name = "destination path (directory/file)", value_hint = ValueHint::FilePath)]
+    #[arg(global = true, help = "Destination file path (optional)", value_hint = ValueHint::FilePath)]
     destination: Option<PathBuf>,
-
-    #[arg(long, value_name = "compressed", default_value_t = false)]
-    compressed: bool,
-
-    #[arg(long, value_name = "hexified", default_value_t = false)]
-    hexified: bool,
 }
 
 fn main() -> Result<(), String> {
-    let Args {
-        injection,
-        source,
-        destination,
-        compressed,
-        hexified,
-    } = Args::parse();
+    let Args { action } = Args::parse();
 
     let calculate_default_destination_file_name = |file_name: &str| {
         let mut file_name = String::from(file_name);
 
-        if injection != Injection::Nothing {
-            file_name = format!("injected_{}", file_name);
-        }
-        if compressed {
-            file_name = format!("compressed_{}", file_name);
-        }
-        if hexified {
-            file_name = format!("hexified_{}.hex", file_name);
+        match &action {
+            Action::Inject {
+                compressed,
+                hexified,
+                ..
+            } => {
+                file_name = format!("injected_{}", file_name);
+                if *compressed {
+                    file_name = format!("compressed_{}", file_name);
+                }
+                if *hexified {
+                    file_name = format!("hexified_{}.hex", file_name);
+                }
+            }
+            Action::Decode { .. } => {
+                file_name = format!("decoded_{}.wasm", file_name);
+            }
         }
 
         file_name
     };
 
-    let destination = match destination {
+    let global_opts = match &action {
+        Action::Inject { global_opts, .. } | Action::Decode { global_opts } => global_opts.clone(),
+    };
+
+    let destination = match global_opts.destination {
         // Creates a new file with the destination as name
         Some(destination_file) => destination_file,
 
         // There is no destination:
         // put it next to the source, surrounding it with the appropriate modifiers
-        None => modify_file_name(source.as_path(), calculate_default_destination_file_name)?,
+        None => modify_file_name(
+            global_opts.source.as_path(),
+            calculate_default_destination_file_name,
+        )?,
     };
 
-    // Get the module
-    let mut module = load_module_from_wasm(source.as_path())?;
+    // // Get the module
+    let mut module = load_module_from_wasm(global_opts.source.as_path())?;
 
-    // "Inject" the module
-    injection.inject(&mut module)?;
+    match action {
+        Action::Inject {
+            injection,
+            compressed,
+            hexified,
+            global_opts: _,
+        } => {
+            // "Inject" the module
+            injection.inject(&mut module)?;
 
-    // Save the modified module
-    save_module_to_wasm(module, destination.as_path(), compressed, hexified)?;
+            // Save the modified module
+            save_module_to_wasm(module, destination.as_path(), compressed, hexified)?;
+        }
+        Action::Decode { .. } => {
+            // Save the modified module
+            save_module_to_wasm(module, destination.as_path(), false, false)?;
+        }
+    }
 
     Ok(())
 }
