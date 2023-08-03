5ee9079747aaa958ae1532f5ce3139484c80c49f
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2023-08-03 17:38:39 +0300

    Make the julia script to execute tests for a specific host only
    Signed-off-by: Maksim Dimitrov <dimitrov.maksim@gmail.com>

--- scripts/runTests.jl
+++ scripts/runTests.jl
@@ -7,64 +7,62 @@ ENV["PATH"] *= ":$(pwd())/bin"
 # Tests directory
 tests_dir::String = get(ENV ,"ZN_TESTS", "./tests")
 
-# Test output path
-tests_output::String = get(ENV, "ZN_TEST_OUTPUT", "./tests_output")
-
-# Filter out the non-`.zndsl` files, just to be safe
-tests::Vector{String} = filter(file -> endswith(file, ".zndsl"), readdir(tests_dir))
-
 # Arrays to accumulate passed and failed test names
 passed_tests::Vector{String} = []
 failed_tests::Vector{String} = []
 
-# Helper for piping both outputs
-function redirect_stdout_stderr(dofunc, outfile, errfile)
-    open(outfile, "w") do out
-        open(errfile, "w") do err
-            redirect_stdout(out) do
-                redirect_stderr(err) do
-                    dofunc()
-                end
+function run_host_tests(host::String)
+    println("Running tests for host $(host)")
+    # Get host tests directory
+    host_tests_dir::String = joinpath(tests_dir, host)
+    # Filter out the non-`.zndsl` files for each host
+    tests::Vector{String} = filter(file -> endswith(file, ".zndsl"), readdir(host_tests_dir))
+    
+    # ... run each test
+    for test::String in tests
+        if endswith(test, ".zndsl")
+            # Extract the index and test name from the test path
+            match_captures = match(r"(\d+)-(.*?)\.zndsl", basename(test)).captures
+            index::Int64 = parse(Int64, match_captures[1])
+            test_name::String = match_captures[2]
+
+            # Prepare the `zombienet test` command
+            command::Cmd = `zombienet -p native test $(joinpath(host_tests_dir, test))`
+            full_test_name::String = "[$(host)] $(test_name)"
+            println("Running test $(full_test_name)")
+
+            # Try to run the test
+            try
+                # Try to run the command
+                run(command)
+
+                # Add the test name to the passed tests array
+                push!(passed_tests, full_test_name)
+            catch e
+                println(e)
+                # Add the test name to the failed tests array
+                push!(failed_tests, full_test_name)
             end
         end
     end
 end
 
-# For each host ...
-for host::String in hosts
-    withenv("ZOMBIENET_DEFAULT_START_COMMAND" => host) do
-        # ... run each test
-        for test::String in tests
-            if endswith(test, ".zndsl")
-                # Extract the index and test name from the test path
-                match_captures = match(r"(\d+)-(.*?)\.zndsl", basename(test)).captures
-                index::Int64 = parse(Int64, match_captures[1])
-                test_name::String = match_captures[2]
-
-                # Prepare the `zombienet test` command
-                command::Cmd = `zombienet -p native test $(joinpath(tests_dir, test))`
-                println("Running test [$(host)] $(test_name)")
-
-                # Try to run the test
-                try
-                    # Create the output files
-                    output_stdout = joinpath(tests_output, "output_$(index)-$(test_name)_stdout.log")
-                    output_stderr = joinpath(tests_output, "output_$(index)-$(test_name)_stderr.log")
-
-                    # Try to run the command and capture its output
-                    redirect_stdout_stderr(output_stdout, output_stderr) do
-                        run(command)
-                    end
-
-                    # Add the test name to the passed tests array
-                    push!(passed_tests, test_name)
-                catch e
-                    # Add the test name to the failed tests array
-                    push!(failed_tests, test_name)
-                end
-            end
+if isempty(ARGS)
+    # For each host ...
+    for host::String in hosts
+        withenv("ZOMBIENET_DEFAULT_START_COMMAND" => host) do
+            run_host_tests(host)
         end
     end
+else
+    # Get the host from the command line arguments
+    host::String = getindex(ARGS, 1)
+    if (length(findall( x -> x == host, hosts )) == 0)
+        println("Host `$(host)` not supported.")
+        exit(1)
+    end
+    # Run the tests for the specified host
+    run_host_tests(host)
 end
 
 # Report the passed and failed tests
@@ -73,3 +71,8 @@ println(passed_tests)
 
 println("Failed tests:")
 println(failed_tests)
+
+# Exit with an error code if any tests failed
+if length(failed_tests) > 0
+    exit(1)
+end
