0c44687541311462ebc2c2d6dfcc052b57924e60
Mihail Kirov <mihail.kirov@limechain.tech>
2023-08-08 13:40:00 +0200

    chore: cleanup unused files

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-dir="${1}"
-file="${2}"
-
-cargo run -- "${dir}" "${file}"
-scp "${dir}/hexified_injected_${file}.hex" zt-node:wasm
--- scripts/hexify.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-const file = fs.readFileSync(process.argv[2]);
-const buff = Buffer.from(file);
-console.log(`0x${buff.toString("hex")}`);
